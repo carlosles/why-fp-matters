2d27c227c1cb18e406ef0ae9ac5c46530b929fe1
CarlosLes <CarlosLes@users.noreply.github.com>
2023-11-10 14:26:31 +0800

    add utility function to filter consecutive values within tolerance

--- src/whyfp/util.py
+++ src/whyfp/util.py
@@ -1,5 +1,6 @@
 """Utilities shared between various modules."""
 from collections.abc import Callable, Iterable
+from itertools import pairwise
 from typing import Any
 
 
@@ -7,3 +8,8 @@ def repeat_fn(func: Callable[[Any], Any], value: Any) -> Iterable[Any]:
     """Repeat function application recursively and indefinitely."""
     yield value
     yield from repeat_fn(func, func(value))
+
+
+def within(values: Iterable[float], tol: float) -> Iterable[float]:
+    """Yield values that differ by no more than tolerance ``tol`` between two successive values."""
+    return (y for x, y in pairwise(values) if abs(x - y) <= tol)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,10 +1,11 @@
 from functools import partial
-from itertools import accumulate, islice, repeat
+from itertools import accumulate, compress, islice, repeat
 from operator import add
 
 from hypothesis import given, strategies as st
+from more_itertools import difference, iequals
 
-from whyfp.util import repeat_fn
+from whyfp.util import repeat_fn, within
 
 
 @given(st.integers())
@@ -13,3 +14,10 @@ def test_repeat_func(initial: int):
     expected = accumulate(repeat(to_add), func=add, initial=initial)
     series = repeat_fn(partial(add, to_add), initial)
     assert all(x == y for x, y in islice(zip(series, expected, strict=True), 100))
+
+
+@given(st.lists(st.floats()), st.floats(min_value=0))
+def test_within(values: list[float], tolerance: float):
+    is_within = (ii > 0 and abs(d) <= tolerance for ii, d in enumerate(difference(values)))
+    expected = compress(values, is_within)
+    assert iequals(within(iter(values), tolerance), expected)
