250bced4dba560ecfe8b4c5f9f11c2b1dbdc874f
CarlosLes <CarlosLes@users.noreply.github.com>
2023-11-10 18:08:29 +0800

    add top-level API for differentiation module

---
+++ src/whyfp/differentiation.py
@@ -0,0 +1,10 @@
+"""Algorithm for numerical differentiation of real-valued functions."""
+from collections.abc import Callable
+
+
+def differentiate(func: Callable[[float], float], x: float, h: float) -> float:
+    pass
+
+
+def easy_diff(func: Callable[[float], float], x: float, h: float) -> float:
+    pass
