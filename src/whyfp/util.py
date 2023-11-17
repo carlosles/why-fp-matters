a9c3752797b5423d0cbd0cb6bbbe62ec436a702f
CarlosLes <CarlosLes@users.noreply.github.com>
2023-11-17 14:06:32 +0800

    complete implementation of differentiation of functions

--- src/whyfp/util.py
+++ src/whyfp/util.py
@@ -1,10 +1,13 @@
 """Utilities shared between various modules."""
 from collections.abc import Callable, Iterable
-from itertools import pairwise
-from typing import TypeVar
+from itertools import islice, pairwise
+import math
+from typing import TypeVar, cast
 
+from more_itertools import nth
 
 T = TypeVar('T')
+T_co = TypeVar('T_co', covariant=True)
 
 
 def repeat_fn(func: Callable[[T], T], value: T) -> Iterable[T]:
@@ -21,3 +24,29 @@ def within(values: Iterable[float], tol: float) -> Iterable[float]:
 def relative(values: Iterable[float], tol: float) -> Iterable[float]:
     """Yield values that differ by no more than relative tolerance ``tol`` between successive values."""
     return (y for x, y in pairwise(values) if abs(x / y - 1) <= tol)
+
+
+def fastest(values: Iterable[float]) -> Iterable[float]:
+    """Yield sequence of approximations computed using a progressively higher order scheme."""
+    return map(second, repeat_fn(improve, values))
+
+
+def improve(values: Iterable[float]) -> Iterable[float]:
+    """Yield improved sequence of approximations."""
+    return elim_error(order(values), values)
+
+
+def order(values: Iterable[float]) -> int:
+    """Return estimate of power exponent that characterizes sequence of approximations."""
+    a, b, c = islice(values, 3)
+    return round(math.log2((a - c) / (b - c) - 1))
+
+
+def elim_error(n: int, values: Iterable[float]) -> Iterable[float]:
+    """Yield values with error terms eliminated from the sequence of successive approximations."""
+    return ((b * 2**n - a) / (2**n - 1) for a, b in pairwise(values))
+
+
+def second(values: Iterable[T]) -> T:
+    """Return second item from sequence of values."""
+    return cast(T, nth(values, 1))
